279e143ef38cfbc83b70f8dbcd06d2d9e594ca8d
KavinPrasad2948 <kavinprasad2948@gmail.com>
2024-04-17 19:08:51 +0530

    :sparkles:Shining bright with fresh enhancements!

--- src/Pages/ProductCard.jsx
+++ src/Pages/ProductCard.jsx
@@ -15,18 +15,20 @@ export default function ProductCard() {
     <div className="cart-container">
       <div className="row d-flex justify-content-center ">
         {productList.map((product) => (
-          <div key={product.id} className="col-md-4 col-md-2 mb-4">
+          <div key={product.id} className="col-md-4  mb-4">
             <Link
               to={`/product/${product.id}`}
               className="text-decoration-none"
             >
               <div className="card">
+                <div className="card-header">
                 <img
                   src={product.images[0]}
                   className="card-img-top img-fluid"
                   alt={product.title}
                   style={{ display: "block" }}
                 />
+                </div>
                 <div className="card-body">
                   <h5 className="card-title">{product.title}</h5>
                   <p className="card-text" style={{ marginBottom: "1rem" }}>
